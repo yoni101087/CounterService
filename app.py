2d63c20ab0d830025cebabebd8394cd35c99fd24
Jonathan Feldamn <yoni101087@gmail.com>
2023-09-30 13:18:33 +0000

    fixed metrics

--- app.py
+++ app.py
@@ -4,17 +4,22 @@ from prometheus_flask_exporter import PrometheusMetrics
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
 
-counter = 0
+counter = metrics.counter(
+    'custom_counter', 'A custom counter metric for the Flask app'
+)
 
 @app.route('/', methods=['GET', 'POST'])
 def counter_service():
-    global counter
-
     if request.method == 'POST':
-        counter += 1
-        return 'POST request served. Current count: {}'.format(counter)
+        counter.inc()
+        return 'POST request served. Current count: {}'.format(counter.get())
     elif request.method == 'GET':
-        return 'Current count: {}'.format(counter)
+        return 'Current count: {}'.format(counter.get())
+
+# Add a route for Prometheus metrics
+@app.route('/metrics')
+def prometheus_metrics():
+    return metrics.export()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
