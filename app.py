e6c536ce1664a6614ea05d248ce62fa43985bb96
Jonathan Feldamn <yoni101087@gmail.com>
2023-09-30 13:37:40 +0000

    chatgpt error2

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-from flask import Flask, request, Response
+from flask import Flask, request
 from prometheus_flask_exporter import PrometheusMetrics
-from prometheus_client import generate_latest, CollectorRegistry
 
 app = Flask(__name__)
-metrics = PrometheusMetrics(app)
+metrics = PrometheusMetrics(app, path='/metrics')  
 
 counter = metrics.counter(
     'custom_counter', 'A custom counter metric for the Flask app'
@@ -16,13 +15,6 @@ def counter_service():
         return 'POST request served. Current count: {}'.format(counter.get())
     elif request.method == 'GET':
         return 'Current count: {}'.format(counter.get())
-
-# Add a route for Prometheus metrics
-@app.route('/metrics')
-def prometheus_metrics():
-    registry = CollectorRegistry(auto_describe=True)
-    data = generate_latest(registry)
-    return Response(data, content_type='text/plain; version=0.0.4')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
