c13a1bc403df8234b278078fa35efd17453a9060
Jonathan Feldamn <yoni101087@gmail.com>
2023-09-30 13:30:18 +0000

    test chatgpt metrics error

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, request
+from flask import Flask, request, Response
 from prometheus_flask_exporter import PrometheusMetrics
+from prometheus_client import generate_latest, CollectorRegistry
 
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
@@ -8,11 +9,6 @@ counter = metrics.counter(
     'custom_counter', 'A custom counter metric for the Flask app'
 )
 
-# Add a route for Prometheus metrics
-@app.route('/metrics')
-def prometheus_metrics():
-    return metrics.export()
-
 @app.route('/', methods=['GET', 'POST'])
 def counter_service():
     if request.method == 'POST':
@@ -20,6 +16,13 @@ def counter_service():
         return 'POST request served. Current count: {}'.format(counter.get())
     elif request.method == 'GET':
         return 'Current count: {}'.format(counter.get())
+
+# Add a route for Prometheus metrics
+@app.route('/metrics')
+def prometheus_metrics():
+    registry = CollectorRegistry(auto_describe=True)
+    data = generate_latest(registry)
+    return Response(data, content_type='text/plain; version=0.0.4')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
