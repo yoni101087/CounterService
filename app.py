80a12793aff9d296b52c96df4ce050528b059007
Jonathan Feldamn <yoni101087@gmail.com>
2023-09-30 12:04:16 +0000

    changes to apply prometheus

--- app.py
+++ app.py
@@ -1,34 +1,20 @@
 from flask import Flask, request
-from prometheus_client import start_http_server, Counter
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
-counter = 0
+metrics = PrometheusMetrics(app)
 
-# Create a Counter metric for request counts
-requests_counter = Counter('my_microservice_requests_total', 'Total number of requests received by the microservice')
+counter = 0
 
 @app.route('/', methods=['GET', 'POST'])
 def counter_service():
     global counter
-    
+
     if request.method == 'POST':
         counter += 1
-        # Increment the requests_counter metric when handling a POST request
-        requests_counter.inc()
         return 'POST request served. Current count: {}'.format(counter)
     elif request.method == 'GET':
-        # Increment the requests_counter metric when handling a GET request
-        requests_counter.inc()
         return 'Current count: {}'.format(counter)
 
-@app.route('/metrics')
-def metrics():
-    # Your metric collection code here
-    return 'Metrics will be displayed here.'
-
 if __name__ == '__main__':
-    # Start an HTTP server to expose Prometheus metrics on port 8000
-    start_http_server(80)
-    
-    # Start your Flask app on port 80
     app.run(host='0.0.0.0', port=80)
